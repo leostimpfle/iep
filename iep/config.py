9c105ce0e58e31d8f4fdd89949683428a8bae01b
leostimpfle <leonardstimpfle@icloud.com>
2026-04-06 17:11:25 +0200

    Add v16

--- iep/config.py
+++ iep/config.py
@@ -3,4 +3,4 @@ from typing import Final
 
 PATH_PACKAGE: Final[Path] = Path(__file__).resolve().parent
 PATH_INPUT: Final[Path] = PATH_PACKAGE.parent / "input"
-VERSION: Final[str] = "v15"
+VERSION: Final[str] = "v16"
